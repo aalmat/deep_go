2ab1cbb5b77ade3fff787f6f8d1620fb6ebb1b9c
aalmat <madiyarbek.a2004@gmail.com>
2025-05-30 23:42:14 +0500

    rw mutext

--- homework/sync_primitives/homework_test.go
+++ homework/sync_primitives/homework_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -9,24 +10,73 @@ import (
 )
 
 type RWMutex struct {
-	// need to implement
+	mu        sync.Mutex
+	readCond  *sync.Cond
+	writeCond *sync.Cond
+
+	readers        uint64
+	writers        uint64
+	waitingWriters uint64
+	writing        bool
+
+	once sync.Once
+}
+
+func (m *RWMutex) init() {
+	m.readCond = sync.NewCond(&m.mu)
+	m.writeCond = sync.NewCond(&m.mu)
 }
 
 func (m *RWMutex) Lock() {
-	// need to implement
+	m.once.Do(m.init)
+	m.mu.Lock()
+
+	m.waitingWriters++
+
+	for m.readers > 0 || m.writing {
+		m.writeCond.Wait()
+	}
+
+	m.waitingWriters--
+	m.writing = true
+	m.mu.Unlock()
 }
 
 func (m *RWMutex) Unlock() {
-	// need to implement
+	m.once.Do(m.init)
+
+	m.mu.Lock()
+	m.writing = false
+	if m.waitingWriters > 0 {
+		m.writeCond.Signal()
+	} else {
+		m.readCond.Broadcast()
+	}
+	m.mu.Unlock()
 }
 
 func (m *RWMutex) RLock() {
-	// need to implement
+	m.once.Do(m.init)
 
+	m.mu.Lock()
+	for m.waitingWriters > 0 || m.writing {
+		m.readCond.Wait()
+	}
+
+	m.readers++
+	m.mu.Unlock()
 }
 
 func (m *RWMutex) RUnlock() {
-	// need to implement
+	m.once.Do(m.init)
+
+	m.mu.Lock()
+	m.readers--
+
+	if m.readers == 0 && m.waitingWriters > 0 {
+		m.writeCond.Signal()
+	}
+	m.mu.Unlock()
 }
 
 func TestRWMutexWithWriter(t *testing.T) {
